2d3bc5dd64cf9d83eae70d59159d4b9bcaf8901f
psitsha <sitsha.polite@yahoo.ie>
2024-04-18 23:56:39 +0100

    Setting up django project

---
+++ backend/Python_Django_HTML_AI/ai_blog_article_generator/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class AiBlogArticleGeneratorConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'ai_blog_article_generator'
